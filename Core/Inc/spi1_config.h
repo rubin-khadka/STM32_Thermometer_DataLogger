d3d043ab499992100a1d1c0907fd32cab5f688b8
rubin-khadka <rubin.robotic@gmail.com>
2025-12-04 18:37:32 +0100

    this spi file will have configuration to run shift register and 7 segmetn display

---
+++ Core/Inc/spi1_config.h
@@ -0,0 +1,21 @@
+/*
+ * spi_config.h
+ *
+ *  Created on: Dec 4, 2025
+ *      Author: Rubin Khadka
+ */
+
+#ifndef INC_SPI1_CONFIG_H_
+#define INC_SPI1_CONFIG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_SPI1_CONFIG_H_ */
---
+++ Core/Src/spi1_config.c
@@ -0,0 +1,9 @@
+/*
+ * spi1_config.c
+ *
+ *  Created on: Dec 4, 2025
+ *      Author: Rubin Khadka
+ */
+
+
+// Shift Registers
