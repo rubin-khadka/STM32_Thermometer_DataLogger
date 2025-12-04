57a435879d3e5b29a2250e8603d7484cf58e2b78
rubin-khadka <rubin.robotic@gmail.com>
2025-12-04 18:36:32 +0100

    added eeprom file, using custom fucntions stead of hal

---
+++ Core/Inc/eeprom_AT24C256.h
@@ -0,0 +1,21 @@
+/*
+ * eeprom_AT24C256.h
+ *
+ *  Created on: Dec 1, 2025
+ *      Author: Rubin Khadka
+ */
+
+#ifndef INC_EEPROM_AT24C256_H_
+#define INC_EEPROM_AT24C256_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_EEPROM_AT24C256_H_ */
---
+++ Core/Src/eeprom_AT24C256.c
@@ -0,0 +1,8 @@
+/*
+ * eeprom_AT24C256.c
+ *
+ *  Created on: Dec 1, 2025
+ *      Author: Rubin Khadka
+ */
+
+
