3751a7c070f8b4c819abd883387eadce35a6098a
rubin-khadka <rubin.robotic@gmail.com>
2025-12-04 18:39:46 +0100

    this second spi wiill contain config for memory card module

---
+++ Core/Inc/spi2_config.h
@@ -0,0 +1,21 @@
+/*
+ * spi2_config.h
+ *
+ *  Created on: Dec 4, 2025
+ *      Author: Rubin Khadka
+ */
+
+#ifndef INC_SPI2_CONFIG_H_
+#define INC_SPI2_CONFIG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_SPI2_CONFIG_H_ */
---
+++ Core/Src/spi2_config.c
@@ -0,0 +1,9 @@
+/*
+ * spi2_config.c
+ *
+ *  Created on: Dec 4, 2025
+ *      Author: Rubin Khadka
+ */
+
+// Memory Card Module
+
