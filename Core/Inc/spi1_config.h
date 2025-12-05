5ac7930b15607fbd257268671f97562f3a2d6781
rubin-khadka <rubin.robotic@gmail.com>
2025-12-05 15:26:12 +0100

    configured spi1 to run with shift registers

--- Core/Inc/spi1_config.h
+++ Core/Inc/spi1_config.h
@@ -8,11 +8,19 @@
 #ifndef INC_SPI1_CONFIG_H_
 #define INC_SPI1_CONFIG_H_
 
+#include "stdint.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/* Macros */
+#define LATCH(x) (x==1 ? (HAL_GPIO_WritePin(SPI_SS_GPIO_Port, SPI_SS_Pin, GPIO_PIN_SET)):\
+                         (HAL_GPIO_WritePin(SPI_SS_GPIO_Port, SPI_SS_Pin, GPIO_PIN_RESET)))
 
+/* Function Prototypes */
+void SPI1_init(void);
+void SPI1_Transmit16(uint16_t data);
 
 #ifdef __cplusplus
 }
--- Core/Src/spi1_config.c
+++ Core/Src/spi1_config.c
@@ -5,5 +5,28 @@
  *      Author: Rubin Khadka
  */
 
+#include "spi1_config.h"
+#include "main.h"
 
-// Shift Registers
+void SPI1_init(void)
+{
+  SPI1->CR1 |= SPI_CR1_SPE; // Enable SPI
+}
+
+void SPI1_Transmit16(uint16_t data)
+{
+  // Wait until TX buffer is empty
+  while (!(SPI1->SR & SPI_SR_TXE)) {
+  }
+
+  // Write 16-bit data to Data Register
+  SPI1->DR = data;
+
+  // Wait for transmission to complete
+  while (SPI1->SR & SPI_SR_BSY) {
+  }
+
+  // Clear RX buffer by reading DR
+  volatile uint16_t dummy = SPI1->DR;
+  (void)dummy;
+}
