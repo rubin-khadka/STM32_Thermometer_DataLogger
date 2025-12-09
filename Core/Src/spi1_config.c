3575d745fb94c5669e8eec25a465707eb01948ab
rubin-khadka <rubin.robotic@gmail.com>
2025-12-09 11:13:35 +0100

    made spi1 register level control, removed cubemx init

--- Core/Src/spi1_config.c
+++ Core/Src/spi1_config.c
@@ -10,23 +10,60 @@
 
 void SPI1_init(void)
 {
-  SPI1->CR1 |= SPI_CR1_SPE; // Enable SPI
+  // Enable Peripheral Clock
+  RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+
+  // Disable SPI to configure it
+  SPI1->CR1 &= ~SPI_CR1_SPE;
+
+  // Reset Control Registers
+  SPI1->CR1 = 0;
+  SPI1->CR2 = 0;
+
+  // Select Master Mode
+  SPI1->CR1 |= SPI_CR1_MSTR;
+
+  // Select Baud Rate
+  SPI1->CR1 |= (0x1 << 3);
+
+  // Software NSS Management
+  SPI1->CR1 |= SPI_CR1_SSM;
+  SPI1->CR1 |= SPI_CR1_SSI;
+
+  // Data Format Mode
+  SPI1->CR1 |= SPI_CR1_DFF;
+
+  // MSB First
+  SPI1->CR1 &= ~SPI_CR1_LSBFIRST;
+
+  // Mode 1: CPOL=0, CPHA=1
+  SPI1->CR1 &= ~SPI_CR1_CPOL;
+  SPI1->CR1 &= ~SPI_CR1_CPHA;
+
+  // Enable SPI
+  SPI1->CR1 |= SPI_CR1_SPE;
+
+  // Dummy read to clear status flags
+  (void) SPI1->SR;
+  (void) SPI1->DR;
+
+  // Wait for SPI to be ready
+  while(SPI1->SR & SPI_SR_BSY);
 }
 
 void SPI1_Transmit16(uint16_t data)
 {
   // Wait until TX buffer is empty
-  while (!(SPI1->SR & SPI_SR_TXE)) {
-  }
+  while(!(SPI1->SR & SPI_SR_TXE));
 
   // Write 16-bit data to Data Register
   SPI1->DR = data;
 
   // Wait for transmission to complete
-  while (SPI1->SR & SPI_SR_BSY) {
-  }
+  while(SPI1->SR & SPI_SR_BSY);
 
-  // Clear RX buffer by reading DR
-  volatile uint16_t dummy = SPI1->DR;
-  (void)dummy;
+  // Clear RX buffer
+  if(SPI1->SR & SPI_SR_RXNE) {
+    (void)SPI1->DR;
+  }
 }
